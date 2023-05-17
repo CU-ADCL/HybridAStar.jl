95bfc6508a5b4ce874c39b32363e94afa186a418
himanshugupta1009 <himanshu.gupta@colorado.edu>
2023-05-16 21:42:07 -0600

    Move example code to a separate folder

---
+++ examples/demo_gridworld.jl
@@ -0,0 +1,36 @@
+using LinearAlgebra
+using StaticArrays: SA
+using DomainSets: Disk
+using HybridAStar: hybrid_astar_search
+
+dynamics(state, action) = state + SA[cosd(action), sind(action)]
+
+node_key(state) = round.(Int, state)
+
+function node_cost(old_state, new_state, action, depth)
+    if new_state in Disk(2.0, SA[5.0, 8.0])
+        return Inf
+
+    end
+    return 1.0
+end
+
+heuristic_cost(state) = norm(state - goal_center)
+
+start_state = SA[2.0, 2.0]
+goal_center = SA[9.0, 9.0]
+actions = [0, 30, 60, 90, 120, 150, 180] # angles that the vehicle can travel
+g = Disk(1.0, goal_center)
+cs = hybrid_astar_search(g, start_state, actions, dynamics, node_key, node_cost, heuristic_cost)
+p = get_path(start_state, cs, dynamics)
+
+#=
+x = [s[1] for s in p]; y = [s[2] for s in p]
+plot(x,y)
+
+using JET
+using BenchmarkTools
+@report_opt hybrid_astar_search(g, holonomic_vs, holonomic_va, holonomic_vehicle_dynamics, get_node_key, nc, hc)
+@profiler hybrid_astar_search(g, holonomic_vs, holonomic_va, holonomic_vehicle_dynamics, get_node_key, nc, hc)
+@benchmark hybrid_astar_search($g, $holonomic_vs, $holonomic_va, holonomic_vehicle_dynamics, get_node_key, nc, hc)
+=#
